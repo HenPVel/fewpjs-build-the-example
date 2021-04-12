d77811f014a31aacbf9eb0a9e92a25f06e26c49b
Henry <hvelas93@gmail.com>
2021-04-12 18:07:28 -0400

    attempted lab

--- main.js
+++ main.js
@@ -4,6 +4,43 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let errorDiv = document.querySelector("#modal")
+
+window.addEventListener('DOMContentLoaded', event => {
+  errorDiv.hidden = true
+
+  function heartChanger(event) {
+    const aHeart = event.target
+    console.log(aHeart)
+    mimicServerCall(url="http://mimicServer.example.com", config={})
+    .then(response => {
+      if (response.ok && aHeart.textContent == EMPTY_HEART ) {
+        aHeart.textContent = FULL_HEART
+      } else if (response.ok && aHeart.textContent== FULL_HEART) {
+        aHeart.textContent = EMPTY_HEART
+      } else {
+      throw new Error(`Your loss, my friend.`) }
+    })
+    .catch(error => {
+      alert(error.message)
+    })
+    
+     
+  }
+  
+  const parentElmArticleList = document.querySelectorAll("article.media-post")
+  
+  
+  
+  for (i=0; i < parentElmArticleList.length; i++) {
+    parentElmArticleList[i].addEventListener('click', heartChanger )
+  }
+}
+
+)
+
+
+
 
 
 
